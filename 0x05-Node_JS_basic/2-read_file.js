e43d0726d7c0161f1d7f34c673b5cb48cade2d70
apha-pesso <mutiuayo99@gmail.com>
2023-05-16 21:47:38 +0100

    Readfile synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,46 @@
+// Reading a file synchronously
+const fs = require("fs");
+
+function countStudents(path) {
+  //   fs.readFile(path, (err, data) => {
+  //     if (err) {
+  //       throw new Error("Cannot load the database");
+  //     }
+
+  //     console.log(data.toString());
+  //   });
+  const data = fs.readFileSync(path);
+  if (!data) {
+    throw new Error("Cannot load the database");
+  }
+  console.log(data.toString());
+  name_list = data
+    .toString()
+    .split("\n")
+    .filter((name) => name.length > 0);
+  console.log(name_list);
+  const students = name_list.length - 1;
+  console.log(`Number of students: ${students}`);
+
+  const CSname = [];
+  const SWEname = [];
+
+  name_list.forEach((element) => {
+    if (element.includes("CS")) {
+      CSname.push(element.split(",")[0]);
+    } else if (element.includes("SWE")) {
+      SWEname.push(element.split(",")[0]);
+    }
+  });
+
+  console.log(
+    `Number of students in CS: ${CSname.length}. List: ${CSname.join(", ")}`
+  );
+  console.log(
+    `Number of students in SWE: ${SWEname.length}. List: ${SWEname.join(", ")}`
+  );
+  //   console.log(CSname);
+  //   console.log(SWEname);
+}
+
+module.exports = countStudents;
