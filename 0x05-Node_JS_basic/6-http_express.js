e60c45f1a4cbba470119ec9b56d5d9f65679d027
apha-pesso <mutiuayo99@gmail.com>
2023-05-17 00:35:15 +0100

    Create server with express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+// Server with express
+
+const express = require('express');
+
+const app = express();
+const hostname = '127.0.0.1';
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
