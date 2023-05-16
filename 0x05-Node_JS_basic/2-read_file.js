2030927fcd627b38378de2d9abaea11c086f0f6b
apha-pesso <mutiuayo99@gmail.com>
2023-05-16 22:05:00 +0100

    Define nameList and reformat

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
 // Reading a file synchronously
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   //   fs.readFile(path, (err, data) => {
   //     if (err) {
   //       throw new Error("Cannot load the database");
@@ -9,40 +9,44 @@ function countStudents (path) {
 
   //     console.log(data.toString());
   //   });
-  const data = fs.readFileSync(path);
-  // if (!data) {
+  try {
+    const data = fs.readFileSync(path);
+    // if (!data) {
     // throw Error('Cannot load the database');
-  // }
-	if (data){
-  // console.log(data.toString());
-  nameList = data
-    .toString()
-    .split('\n')
-    .filter((name) => name.length > 0);
-  // console.log(name_list);
-  const students = nameList.length - 1;
-  console.log(`Number of students: ${students}`);
+    // }
 
-  const CSname = [];
-  const SWEname = [];
+    // console.log(data.toString());
+    const nameList = data
+      .toString()
+      .split('\n')
+      .filter((name) => name.length > 0);
+    // console.log(name_list);
+    const students = nameList.length - 1;
+    console.log(`Number of students: ${students}`);
 
-  nameList.forEach((element) => {
-    if (element.includes('CS')) {
-      CSname.push(element.split(',')[0]);
-    } else if (element.includes('SWE')) {
-      SWEname.push(element.split(',')[0]);
-    }
-  });
+    const CSname = [];
+    const SWEname = [];
 
-  console.log(
-    `Number of students in CS: ${CSname.length}. List: ${CSname.join(', ')}`
-  );
-  console.log(
-    `Number of students in SWE: ${SWEname.length}. List: ${SWEname.join(', ')}`
-  );
-	}else {
-		throw Error('Cannot load the database');
-	}
+    nameList.forEach((element) => {
+      if (element.includes('CS')) {
+        CSname.push(element.split(',')[0]);
+      } else if (element.includes('SWE')) {
+        SWEname.push(element.split(',')[0]);
+      }
+    });
+
+    console.log(
+      `Number of students in CS: ${CSname.length}. List: ${CSname.join(', ')}`
+    );
+    console.log(
+      `Number of students in SWE: ${SWEname.length}. List: ${SWEname.join(
+        ', '
+      )}`
+    );
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+
   //   console.log(CSname);
   //   console.log(SWEname);
 }
