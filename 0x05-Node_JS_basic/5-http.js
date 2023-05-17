e118fa7a58f4e9f0c3eee1e218dcc63c860cb908
apha-pesso <mutiuayo99@gmail.com>
2023-05-17 16:40:37 +0100

    Check

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,7 +3,8 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const readFileAsync = require('./3-read_file_async');
+// const readFileAsync = require('./3-read_file_async');
+const readSync = require('./2-read_file');
 
 const database = process.argv[2];
 
@@ -12,14 +13,33 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    // const response = readSync(database);
+    // res.end(`This is the list of our students\n${response}`);
+    // res.setHeader("Content-Type", "text/plain");
+    // res.end(response);
+
+    let output = '';
+
+    // Override console.log to capture the output
+    const originalLog = console.log;
+    console.log = function (message) {
+      output += `${message}`;
+    };
+
+    readSync(database);
+
+    // Restore the original console.log
+    console.log = originalLog;
+    res.end(`This is the list of our students\n${output}`);
+
     // res.end("This is the list of our students");
-    readFileAsync(database)
-      .then((data) => {
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((err) => {
-        res.end(err);
-      });
+    // readFileAsync(database)
+    //   .then((data) => {
+    //     res.end(`This is the list of our students\n${data}`);
+    //   })
+    //   .catch((err) => {
+    //     res.end(err);
+    //   });
   }
 });
 
