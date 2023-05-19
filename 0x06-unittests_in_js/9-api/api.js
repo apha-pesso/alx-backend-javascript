fb8639c2d9a8368c7ea58909fce364f0bd09f312
apha-pesso <mutiuayo99@gmail.com>
2023-05-19 22:13:51 +0100

    Test API with id

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,24 @@
+// API file
+
+const express = require("express");
+const port = 7865;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id([0-9]+)", (req, res) => {
+  const id = req.params.id;
+  if (isNaN(id)) {
+    res.statusCode = 404;
+    res.send("Not a number");
+  } else {
+    res.statusCode = 200;
+    res.send(`Payment methods for cart ${id}`);
+  }
+});
+
+app.listen(port, () => console.log("API available on localhost port 7865"));
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+// Test API file
+
+const request = require("request");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("GET /", function () {
+  it("Makes call to GET /", function (done) {
+    request("http://localhost:7865", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal("Welcome to the payment system");
+      done();
+    });
+  });
+});
+
+describe("GET /cart/:id", function () {
+  it("call GET /cart/:id and test the statuscode", function (done) {
+    request("http://localhost:7865/cart/12", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal("Payment methods for cart 12");
+      done();
+    });
+  });
+
+  it("call GET /cart/:id and test the statuscode", function (done) {
+    request("http://localhost:7865/cart/hello", (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
